00bef78095d9a71c9202210cb8da63eaa119e75c
Asrorbekrustamovich <asrorrustamovich007@gmail.com>
2025-07-17 16:02:01 +0500

    asdsad

--- app/models.py
+++ app/models.py
@@ -8,16 +8,15 @@ class RoleChoices(models.TextChoices):
     BUXGALTER = "buxgalter", "Buxgalter"
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, full_name, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Email is required")
-        email = self.normalize_email(email)
-        user = self.model(email=email, full_name=full_name, **extra_fields)
+    def create_user(self, username, full_name, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+        user = self.model(username=username, full_name=full_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, full_name, password=None, **extra_fields):
+    def create_superuser(self, username, full_name, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("role", RoleChoices.ADMIN)
@@ -27,21 +26,22 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, full_name, password, **extra_fields)
+        return self.create_user(username, full_name, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
     full_name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
     role = models.CharField(max_length=20, choices=RoleChoices.choices, default=RoleChoices.ENTREPRENEUR)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["full_name"]
 
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return self.username
